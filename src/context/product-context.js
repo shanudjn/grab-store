4a22b21d2620a095b2044907c5d0270291990c84
Md Shahazad Uddin <shanudjn@gmail.com>
2021-03-31 16:32:24 +0530

    added filter and sorting functionality

--- src/context/product-context.js
+++ src/context/product-context.js
@@ -12,20 +12,24 @@ const productList = data;
 export function ProductsProvider({ children }) {
 
     function productReducer(state, action) {
-        switch (action) {
-            case "SORT_BY":
-
+        console.log("inside reducer")
+        switch (action.type) {
+            case "SORT":
+                console.log("Inside Sort Reducer")
                 return { ...state, sortBy: action.payload }
-
+            case "TOGGLE_DELIVERY":
+                return { ...state, showFastDelivery: !state.showFastDelivery }
+            case "TOGGLE_INVENTORY":
+                return { ...state, showAllInventory: !state.showAllInventory }
             default:
                 return state;
         }
     }
+    const [{ showAllInventory, showFastDelivery, sortBy }, productFilterDispatch] = useReducer(productReducer, { showAllInventory: true, showFastDelivery: false, sortBy: null });
 
-    const [{ showAllInventory, showFastDelivery, sortBy }, productFilterDispatch] = useReducer(productReducer, { showAllInventory: true, showFastDelivery: false, sortBy: null })
 
 
-    return <ProductContext.Provider value={{ productList, productFilterDispatch }}>
+    return <ProductContext.Provider value={{ productList, sortBy, showAllInventory, showFastDelivery, productFilterDispatch }}>
         {children}
     </ProductContext.Provider>
 }
--- src/pages/products-listing/products-listing.jsx
+++ src/pages/products-listing/products-listing.jsx
@@ -2,12 +2,12 @@ import './products-listing.css';
 //import data from '../../data';
 
 import { useCart } from '../../context/cart-context';
-import { useProduct, } from '../../context/product-context';
+import { useProduct } from '../../context/product-context';
 
 export function ProductListing() {
 
     const { cartList, wishList, dispatch } = useCart();
-    const { productList } = useProduct();
+    const { productList, sortBy, showAllInventory, showFastDelivery, productFilterDispatch } = useProduct();
 
     function getColor(wishList, id) {
         const index = wishList.findIndex((wishlistItem) => wishlistItem.id === id);
@@ -30,6 +30,30 @@ export function ProductListing() {
         }
         return false;
     }
+
+    function getSortedData(productList, sortBy) {
+        if (sortBy === "HIGH_TO_LOW") {
+            console.log("sorting:high to low")
+            return productList.sort((a, b) => b["price"] - a["price"])
+        }
+        if (sortBy === "LOW_TO_HIGH") {
+            console.log("sorting:low to high")
+            return productList.sort((a, b) => a["price"] - b["price"])
+        }
+        return productList;
+    }
+    function getFilteredData(sortedData, { showAllInventory, showFastDelivery }) {
+        console.log("returning filtered data")
+        return sortedData
+            .filter(({ fastDelivery }) => showFastDelivery ? fastDelivery : true)
+            .filter(({ inStock }) => showAllInventory ? true : inStock)
+    }
+    const sortedData = getSortedData(productList, sortBy);
+
+    const filteredData = getFilteredData(sortedData, { showAllInventory, showFastDelivery })
+
+    console.log(filteredData);
+
     return (
         <>
             <div className="product-section">
@@ -40,6 +64,8 @@ export function ProductListing() {
                             <input
                                 type="radio"
                                 name="sort"
+                                onChange={() => productFilterDispatch({ type: "SORT", payload: "HIGH_TO_LOW" })}
+                                checked={sortBy === "HIGH_TO_LOW"}
                             />
                             Price High to Low
                         </label>
@@ -47,6 +73,8 @@ export function ProductListing() {
                             <input
                                 type="radio"
                                 name="sort"
+                                onChange={() => productFilterDispatch({ type: "SORT", payload: "LOW_TO_HIGH" })}
+                                checked={sortBy === "LOW_TO_HIGH"}
                             />
                             Price Low to High
                         </label>
@@ -55,7 +83,8 @@ export function ProductListing() {
                         <label>
                             <input
                                 type="checkbox"
-
+                                checked={showAllInventory}
+                                onChange={() => productFilterDispatch({ type: "TOGGLE_INVENTORY" })}
                             />
             Include Out of Stock
           </label>
@@ -63,14 +92,15 @@ export function ProductListing() {
                         <label>
                             <input
                                 type="checkbox"
-
+                                checked={showFastDelivery}
+                                onChange={() => productFilterDispatch({ type: "TOGGLE_DELIVERY" })}
                             />
-            Fast Delivery Only
+                            Fast Delivery Only
           </label>
                     </aside>
                 </div>
                 <div className="product-listing">
-                    {productList.map((product) => (
+                    {filteredData.map((product) => (
                         <div className="card card-badge" key={product.id}>
                             <img src={product.image} alt="pic" />
 
