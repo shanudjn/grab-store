170e0f56a6036bf09093e55b8dc82d7f368431ac
Md Shahazad Uddin <shanudjn@gmail.com>
2021-07-22 19:19:40 +0530

    button fix

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -12,8 +12,8 @@ export function Home() {
                     <p>Find merch of your favorite celebrities.</p>
                     <img src={hero} alt="hero-img" />
                 </div>
-
-                <button className="btn btn-primary button-home"><Link to="/products">Shop Now</Link></button>
+                <Link to="/products">
+                    <button className="btn btn-primary button-home">Shop Now</button></Link>
             </div>
         </main >
     )
